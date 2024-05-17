6b80943fb41f755cb979cc2a8c2ee2a7966e7109
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-05-17 17:37:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 function highlight() {
     //Write your code here
-
-
+const boldWords=document.querySelectorAll('p strong');
+	boldWords.forEach(word=>{
+		word.style.color='green';
+	})
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+const boldWords=document.querySelectorAll('p strong');
+	boldWords.forEach(word=>{
+		word.style.color='black'
+	})
 }
